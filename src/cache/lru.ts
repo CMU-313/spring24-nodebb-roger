3b1bc39d2788f6173ab4853e7d4e6ecbdd9a293b
Theo Kroening <theodore@kroening.com>
2024-02-06 22:53:26 -0500

    Push changes from P1!
    Passes the linter and the test suite locally.

--- src/cache/lru.ts
+++ src/cache/lru.ts
@@ -1,40 +1,62 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 // import LRU from 'lru-cache';
-const lru_cache_1 = __importDefault(require("lru-cache"));
+import LRU from 'lru-cache';
 // lru-cache@7 deprecations
-const winston_1 = __importDefault(require("winston"));
-const chalk_1 = __importDefault(require("chalk"));
+import winston from 'winston';
+import chalk from 'chalk';
 // pubsub import should occur after import of chalk
-const pubsub_1 = __importDefault(require("../pubsub"));
-function cacheCreate(opts) {
+import pubsub from '../pubsub';
+
+/* Can extend the key and value types here */
+type keyType = string;
+type valueType = string;
+
+interface CacheBB {
+    name ?: string;
+    hits : number;
+    misses : number;
+    enabled ?: boolean;
+    set : (key : keyType, value : valueType, ttl ?: number) => void;
+    get : (key : keyType) => undefined | valueType;
+    reset : () => void;
+    clear : () => void;
+    del : (keys : keyType[]) => void;
+    delete : (keys : keyType[]) => void;
+    getUnCachedKeys : (keys : keyType[], cachedData : Map<keyType, valueType>) => keyType[];
+    dump : () => [ keyType, LRU.Entry<valueType> ][];
+    peek : (key : keyType) => undefined | valueType;
+}
+type Opts = LRU.Options<keyType, valueType> & {name ?: string; enabled ?: boolean};
+
+function cacheCreate(opts : Opts) {
     // sometimes we kept passing in `length` with no corresponding `maxSize`.
     // This is now enforced in v7; drop superfluous property
     if (opts.hasOwnProperty('length') && !opts.hasOwnProperty('maxSize')) {
-        winston_1.default.warn(`[cache/init(${opts.name})] ${chalk_1.default.white.bgRed.bold('DEPRECATION')} ${chalk_1.default.yellow('length')} was passed in without a corresponding ${chalk_1.default.yellow('maxSize')}. Both are now required as of lru-cache@7.0.0.`);
+        winston.warn(`[cache/init(${opts.name})] ${chalk.white.bgRed.bold('DEPRECATION')} ${chalk.yellow('length')} was passed in without a corresponding ${chalk.yellow('maxSize')}. Both are now required as of lru-cache@7.0.0.`);
         delete opts.length;
     }
-    const deprecations = new Map([
+
+    const deprecations = new Map<string, string>([
         ['stale', 'allowStale'],
         ['maxAge', 'ttl'],
         ['length', 'sizeCalculation'],
     ]);
     deprecations.forEach((newProp, oldProp) => {
         if (opts.hasOwnProperty(oldProp) && !opts.hasOwnProperty(newProp)) {
-            winston_1.default.warn(`[cache/init(${opts.name})] ${chalk_1.default.white.bgRed.bold('DEPRECATION')} The option ${chalk_1.default.yellow(oldProp)} has been deprecated as of lru-cache@7.0.0. Please change this to ${chalk_1.default.yellow(newProp)} instead.`);
+            winston.warn(`[cache/init(${opts.name})] ${chalk.white.bgRed.bold('DEPRECATION')} The option ${chalk.yellow(oldProp)} has been deprecated as of lru-cache@7.0.0. Please change this to ${chalk.yellow(newProp)} instead.`);
             /* Can pull the types of stale, maxAge, and length from the lru-cache documentation */
-            opts[newProp] = opts[oldProp];
+            opts[newProp] = opts[oldProp] as (boolean | number);
             delete opts[oldProp];
         }
     });
-    const lruCache = new lru_cache_1.default(opts);
-    const cache = {};
+
+    const lruCache = new LRU<keyType, valueType>(opts);
+
+    const cache = {} as CacheBB;
     cache.name = opts.name;
     cache.hits = 0;
     cache.misses = 0;
     cache.enabled = opts.hasOwnProperty('enabled') ? opts.enabled : true;
+
     // expose properties while keeping backwards compatibility
     const propertyMap = new Map([
         ['length', 'calculatedSize'],
@@ -48,19 +70,21 @@ function cacheCreate(opts) {
     propertyMap.forEach((lruProp, cacheProp) => {
         Object.defineProperty(cache, cacheProp, {
             get: function () {
-                return lruCache[lruProp];
+                return lruCache[lruProp] as valueType;
             },
             configurable: true,
             enumerable: true,
         });
     });
-    cache.set = function (key, value, ttl) {
+
+    cache.set = function (key, value, ttl ?: number) {
         if (!cache.enabled) {
             return;
         }
         const opts = ttl ? { ttl: ttl } : {};
         lruCache.set.apply(lruCache, [key, value, opts]);
     };
+
     cache.get = function (key) {
         if (!cache.enabled) {
             return undefined;
@@ -68,44 +92,49 @@ function cacheCreate(opts) {
         const data = lruCache.get(key);
         if (data === undefined) {
             cache.misses += 1;
-        }
-        else {
+        } else {
             cache.hits += 1;
         }
         return data;
     };
+
     cache.del = function (keys) {
         if (!Array.isArray(keys)) {
             keys = [keys];
         }
-        pubsub_1.default.publish(`${cache.name}:lruCache:del`, keys);
+        pubsub.publish(`${cache.name}:lruCache:del`, keys);
         keys.forEach(key => lruCache.delete(key));
     };
     cache.delete = cache.del;
+
     function localReset() {
         lruCache.clear();
         cache.hits = 0;
         cache.misses = 0;
     }
+
     cache.reset = function () {
-        pubsub_1.default.publish(`${cache.name}:lruCache:reset`);
+        pubsub.publish(`${cache.name}:lruCache:reset`);
         localReset();
     };
     cache.clear = cache.reset;
-    pubsub_1.default.on(`${cache.name}:lruCache:reset`, () => {
+
+    pubsub.on(`${cache.name}:lruCache:reset`, () => {
         localReset();
     });
-    pubsub_1.default.on(`${cache.name}:lruCache:del`, (keys) => {
+
+    pubsub.on(`${cache.name}:lruCache:del`, (keys : keyType[]) => {
         if (Array.isArray(keys)) {
             keys.forEach(key => lruCache.delete(key));
         }
     });
+
     cache.getUnCachedKeys = function (keys, cachedData) {
         if (!cache.enabled) {
             return keys;
         }
-        let data;
-        let isCached;
+        let data : valueType | undefined;
+        let isCached : boolean;
         const unCachedKeys = keys.filter((key) => {
             data = cache.get(key);
             isCached = data !== undefined;
@@ -114,18 +143,23 @@ function cacheCreate(opts) {
             }
             return !isCached;
         });
+
         const hits = keys.length - unCachedKeys.length;
         const misses = keys.length - hits;
         cache.hits += hits;
         cache.misses += misses;
         return unCachedKeys;
     };
+
     cache.dump = function () {
         return lruCache.dump();
     };
+
     cache.peek = function (key) {
         return lruCache.peek(key);
     };
+
     return cache;
 }
-module.exports = cacheCreate;
+
+export = cacheCreate;
